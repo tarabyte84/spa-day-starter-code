f4a8ad7cfcab8a6f03e8ca8c536f67b85b7e84bf
tarabyte84 <tekotthoff@me.com>
2021-01-13 20:16:59 -0600

    Started working on processAddUserForm

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,7 +1,10 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +13,11 @@ public class UserController {
 
     @GetMapping("/user/add")
     public String displayAddUserForm() {
+        return "redirect:add";
+    }
 
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        // add form submission handling code here
     }
 
 
